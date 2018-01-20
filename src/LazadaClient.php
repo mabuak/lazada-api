601bc31262addd50a3486990f80a8318092e656b
Fachruzi Ramadhan <mabuak@live.com>
2018-01-21 00:54:55 +0700

    add __construct for config

--- src/LazadaClient.php
+++ src/LazadaClient.php
@@ -7,6 +7,37 @@
 
 namespace ramadhan;
 
-class LazadaClient{
+use Exception;
 
+class LazadaClient {
+	
+	const DATE_FORMAT = "Y-m-d\TH:i:s+\\0\\0\\:\\0\\0";
+	
+	private $config = [
+		'userId'  => null,
+		'apiKey'  => null,
+		'apiHost' => null,
+		'version' => 1.0,
+	];
+	
+	public function __construct( array $config ) {
+		
+		foreach ( $config as $key => $value ) {
+			if ( array_key_exists( $key, $this->config ) ) {
+				$this->config[ $key ] = $value;
+			}
+		}
+		
+		$required_keys = [
+			'userId',
+			'apiKey',
+			'apiHost'
+		];
+		
+		foreach ( $required_keys as $key ) {
+			if ( is_null( $this->config[ $key ] ) ) {
+				throw new Exception( 'Required field ' . $key . ' is not set' );
+			}
+		}
+	}
 }
